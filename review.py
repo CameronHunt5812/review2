680efc9f526c4fd254b96d37f8810c885cd6b2a5
CameronHunt5812 <s581267@sdudent.roundRoundRock.isd>
2014-10-23 15:18:55 -0500

    revew lab 2
    done in 2 diffrent loops

--- review.py
+++ review.py
@@ -13,6 +13,16 @@
 # Who do we appreciate?
 
 # Use a loop to complete this, either a for loop or a while loop.
+end = 0
+while (end < 8):
+    end = end + 2
+    print end
+print "Who do we appreciate?"
+
+for num1 in range(2,9,2):
+    print num1
+print "Who do we appreciate?"
+
 # Each time through the loop, print out either 2, 4, 6 or 8.
 # When the loop is finished, print "Who do we appreciate?"
 # Remember how range can be used to create the list [2,4,6,8]
